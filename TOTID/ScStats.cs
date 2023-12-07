59423db34b6ad8bc9dceeefd0e979de9d6d44c58
Hibiz3a <106378987+Hibiz3a@users.noreply.github.com>
2023-12-07 21:52:13 +0100

    Stats Render
    les stats apparaisent dans un menu :
    - damage
    - health
    Point de stats incrementable.

--- TOTID/ScStats.cs
+++ TOTID/ScStats.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScStats : MonoBehaviour
 {
+     
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Text healthText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthText.text = "HEALTH : " + currentHealth;
     }
 }
