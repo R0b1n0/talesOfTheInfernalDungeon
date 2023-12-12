b4296778b06a5b256bcaed349d31d740dcab1548
Lawice <nohlan.chillmoonlight@gmail.com>
2023-12-12 16:29:18 +0100

    Résolution + ratio + finish options

--- TOTID/Assets/Scripts/ScVisualSettings.cs
+++ TOTID/Assets/Scripts/ScVisualSettings.cs
@@ -1,29 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScVisualSettings : MonoBehaviour {
-
     public TMP_Dropdown resolutionDropdown;
-    
+
     Resolution[] resolutions;
 
-    void Start (){
+    void Awake() {
+        Screen.fullScreen=true;
+    }
+
+    void Start() {
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
 
-        List<string> options = new List<string> ();
+        List<string> options = new List<string>();
+        List<string> uniqueResolutions = new List<string>();
+
+        int currentResolutionsIndex = 0;
 
         for (int u = 0; u < resolutions.Length; u++) {
-            string option = resolutions[u].width + " x " +resolutions[u].height;
-            options.Add (option);
+            string resolutionString = resolutions[u].width + " x " + resolutions[u].height;
+
+            if (!uniqueResolutions.Contains(resolutionString)) {
+                options.Add(resolutionString);
+                uniqueResolutions.Add(resolutionString);
+
+                if (resolutions[u].width == Screen.currentResolution.width && resolutions[u].height == Screen.currentResolution.height) {
+                    currentResolutionsIndex = options.Count - 1;
+                }
+            }
         }
 
-        resolutionDropdown.AddOptions (options);
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex) {
+        string resolutionString = resolutionDropdown.options[resolutionIndex].text;
+        string[] resolutionParts = resolutionString.Split('x');
+        int width = int.Parse(resolutionParts[0]);
+        int height = int.Parse(resolutionParts[1]);
+
+        Resolution resolution = new Resolution {
+            width = width,
+            height = height
+        };
+
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetFullscreen (bool isFullscreen) {
