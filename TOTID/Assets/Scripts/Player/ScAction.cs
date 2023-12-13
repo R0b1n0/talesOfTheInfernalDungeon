924e06a7f442e3e83d13e7c107b67c62236efc0b
Lawice <nohlan.chillmoonlight@gmail.com>
2023-12-13 17:16:21 +0100

    highlight

--- TOTID/Assets/Scripts/Player/ScAction.cs
+++ TOTID/Assets/Scripts/Player/ScAction.cs
@@ -1,40 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
-public class ScAction : MonoBehaviour
-{
+public class ScAction : MonoBehaviour {
     [SerializeField] Transform cameraHolder;
     private ScMovement movementScript;
+    private Ray viewRay;
+    private RaycastHit viewHit;
+    private ScWayPoint endPath;
+    public GameObject highlight;
+    public Transform highlightTransform;
 
-    private void Start()
-    {
+    private void Start() {
         movementScript = GetComponent<ScMovement>();
+        viewHit = new RaycastHit();
     }
 
-    public void ActionOnRelease()
-    {
-        Ray ray = new Ray(cameraHolder.position, cameraHolder.forward);
-        RaycastHit hit = new RaycastHit();
-        Physics.Raycast(ray, out hit);
+    private void Update() {
+        viewRay = new Ray(cameraHolder.position, cameraHolder.forward);
+        Physics.Raycast(viewRay, out viewHit);
+        Preview();
+    }
 
-        if (hit.collider != null)
-        {
-            switch (hit.transform.gameObject.layer)
-            {
+    public void ActionOnRelease() {
+        if (viewHit.collider != null) {
+            switch (viewHit.transform.gameObject.layer) {
                 case 6:
-                    List<ScWayPoint> path = hit.transform.GetComponent<ScRoom>().FindPathBetween(hit.point, movementScript.GetCurrentCell());
+                    List<ScWayPoint> path = viewHit.transform.GetComponent<ScRoom>().FindPathBetween(viewHit.point, movementScript.GetCurrentCell());
                     
                     movementScript.SetPath(path);
-
                     
                     break;
             }
         }
     }
 
-    public void ActionOnClick()
-    {
+    public void Preview() {
+
+        if (viewHit.collider != null)
+        {
+            switch (viewHit.transform.gameObject.layer)
+            {
+                case 6:
+                    endPath = viewHit.transform.GetComponent<ScRoom>().FindClossestCell(viewHit.point);
+                    if (endPath != null)
+                    {
+                        highlight.SetActive(true);
+                        highlightTransform.position = endPath.wayPointId;
+
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            highlight.SetActive(false);
+        }
+        
+    }
+
+    public void ActionOnClick() {
 
     }
 }
