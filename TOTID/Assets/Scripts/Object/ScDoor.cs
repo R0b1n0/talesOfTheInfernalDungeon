001effffdffcae0c2af6c3797a2f7c193d20562f
Lawice <nohlan.chillmoonlight@gmail.com>
2023-12-14 16:14:04 +0100

    Porte Update

--- TOTID/Assets/Scripts/Object/ScDoor.cs
+++ TOTID/Assets/Scripts/Object/ScDoor.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScDoor : ScInteractible {
+    private bool isDoorOpen = false;
+    private Transform parentTransform;
 
+    private void Start()
+    {
+        parentTransform = transform.root;
+    }
     public void DoorOpen() {
         Debug.Log("Door Open");
+        isDoorOpen = true;
+        parentTransform.Rotate(0, -90, 0);
     }
+
+    public void DoorClose(){
+        Debug.Log("Door Close");
+        isDoorOpen = false;
+        parentTransform.Rotate(0, 90, 0);
+    }
+
     public override void Interact(){
-        DoorOpen();
+        if (isDoorOpen) { DoorClose(); }
+        else if (!isDoorOpen) { DoorOpen(); }
     }
 }
